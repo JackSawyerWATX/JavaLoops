23e2f51f31155372e7a56e91e046d23189761aac
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-11 14:37:29 -0700

    If Else

---
+++ WelcomeIfElse.java
@@ -0,0 +1,15 @@
+public class WelcomeIfElse {
+    public static void main(String[] args) {
+        int age = 0;
+
+        if (age > 0) {
+            if (age > 21) {
+                System.out.println("Welcome!");
+            } else {
+                System.out.println("Too Young.");
+            }
+        }else {
+            System.out.println("ERROR");
+        }
+    }
+}
