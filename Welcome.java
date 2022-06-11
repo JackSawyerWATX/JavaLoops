6b6b354d5153f1100b762cbb584f9392f8cb3067
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-11 14:24:40 -0700

    Welcome

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome {
+    public static void main(String [] args) {
+        int age = 42;
+
+        if (age < 21) {
+            System.out.println("Too Young.");
+        } else {
+            System.out.println("Welcome!");
+        }
+    }
+}
