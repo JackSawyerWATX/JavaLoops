61017dbefced3192675b54cc7b935d1faf23ea22
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-11 15:07:16 -0700

    Logical Operator

---
+++ LogicalOperator.java
@@ -0,0 +1,12 @@
+public class LogicalOperator {
+    public static void main(String[] args) {
+        int age = 24;
+        double money = 2800;
+
+        if (age > 18) {
+            if (money > 500.00) {
+                System.out.println("Welcome!");
+            }
+        }
+    }
+}
