0059384ded0c875d9152e938ddc96d044a25e051
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-11 15:24:14 -0700

    LO2

---
+++ LogicalOperator2.java
@@ -0,0 +1,12 @@
+public class LogicalOperator2 {
+    public static void main(String[] args) {
+        int age = 24;
+        double money = 250;
+
+        if (age > 18 && money > 500) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Go Away.");
+        }
+    }
+}
