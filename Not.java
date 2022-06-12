7793e703bf665368d1d6a8a0cd3fc1d7ab445853
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-12 15:50:24 -0700

    Not

---
+++ Not.java
@@ -0,0 +1,13 @@
+public class Not {
+    public static void main(String[] args) {
+        
+        int age = 7;
+
+        if (!(age > 18)) {
+            System.out.println("Too Young.");
+        } else {
+            System.out.println("Welcome!");
+        }
+    }
+    
+}
