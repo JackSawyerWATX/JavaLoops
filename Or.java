af7d7635b1c9c3bc870552ad5b85e18a403c8a1f
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-12 15:44:29 -0700

    Or

---
+++ Or.java
@@ -0,0 +1,13 @@
+public class Or {
+    public static void main(String[] args) {
+        
+        int age = 6;
+        int money = 100;
+
+        if (age > 18 || money > 500) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("GTFOutta here!");
+        }
+    }
+}
