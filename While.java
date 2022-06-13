4a2fe81fb6ff78a7854233e1bcca27cad5bc6af8
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-13 07:39:36 -0700

    While

---
+++ While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        int x = 56;
+        
+        while (x > 3) {
+            System.out.println(x);
+            x--;
+        }
+    }
+}
