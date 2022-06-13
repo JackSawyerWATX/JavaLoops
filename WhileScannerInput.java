572c0d946480e139d40903d0460b5d07d226ca08
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-13 08:12:15 -0700

    WhileScanner

---
+++ WhileScannerInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class WhileScannerInput {
+    public static void main(String[] args) {
+        
+        try(Scanner scanner = new Scanner(System.in)){
+        int n = scanner.nextInt();
+
+            for (int i = 0; i < n; i++) {
+                System.out.println("Welcome");
+        }}
+    }
+}
