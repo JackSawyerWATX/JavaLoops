131d72920af916dfffc3141bb8e1354c7d7826cf
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-12 17:44:56 -0700

    SwitchExpressions

---
+++ SwitchExpression.java
@@ -0,0 +1,11 @@
+public class SwitchExpression {
+    public static void main(String[] args) {
+        int day = 7;
+        String dayString = switch(day) {
+            case 1, 2, 3, 4, 5 -> "Working Day";
+            case 6, 7 -> "Weekend";
+            default -> "Invalid Day";
+        };
+        System.out.println(dayString);
+    }
+}
