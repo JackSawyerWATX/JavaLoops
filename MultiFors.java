861c2836df1eceb8602548f4a8d74c283e935bf4
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-13 08:51:33 -0700

    MultiFors

---
+++ MultiFors.java
@@ -0,0 +1,10 @@
+public class MultiFors {
+    public static void main(String[] args) {
+        for (int i = 2; i < 1000000; i = i + i) {
+            System.out.println(i);
+        }
+    }
+}
+//*when I run the loop as (int i = 2; i < 10; i = i * i) but tthe condition is
+//*over 10,000, and the iteration is a * factor, why does it run as an 
+//*infinate loop?
