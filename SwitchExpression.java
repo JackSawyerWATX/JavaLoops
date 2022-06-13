f487ca40a4ee5e13e3eb2a46ddfbc5b4c73c6ce0
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-12 18:03:58 -0700

    SwitchExpressionswithJSON

--- SwitchExpression.java
+++ SwitchExpression.java
@@ -1,6 +1,6 @@
 public class SwitchExpression {
     public static void main(String[] args) {
-        int day = 7;
+        int day = 3;
         String dayString = switch(day) {
             case 1, 2, 3, 4, 5 -> "Working Day";
             case 6, 7 -> "Weekend";
