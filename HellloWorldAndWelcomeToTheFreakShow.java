3b9dec7512f067bb2ab7a1302433d2041dff9099
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-06-14 14:45:40 -0700

    HelloandWelcome

---
+++ HellloWorldAndWelcomeToTheFreakShow.java
@@ -0,0 +1,7 @@
+public class HellloWorldAndWelcomeToTheFreakShow {
+    public static void main(String[] args) {
+        System.out.println("Hello World! ");
+        System.out.println("and ");
+        System.out.println("Welcome to The FREAK SHOW!");
+    }
+}
